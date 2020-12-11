1754a0a4fd5a7ed7c9b6f24fe8caa86b2339b205
Gustavo Miquéias <gumiqueias@hotmail.com>
2020-12-11 15:20:12 -0300

    Projeto integrador

--- ProjetoIntegrador/src/main/java/com/projeto/ecommerce/Repository/RepositoryCategoria.java
+++ ProjetoIntegrador/src/main/java/com/projeto/ecommerce/Repository/RepositoryCategoria.java
@@ -1,12 +1,12 @@
-package com.projeto.ecommerce.Repository;
+package com.projeto.ecommerce.repository;
 
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.projeto.ecommerce.model.CategoriaModel;
+import com.projeto.ecommerce.model.Categoria;
 
-public interface RepositoryCategoria extends JpaRepository <CategoriaModel, Long> {
+public interface RepositoryCategoria extends JpaRepository <Categoria, Long> {
 
-	public List <CategoriaModel> findAllByTipoContainingIgnoreCase(String tipo);
+	public List <Categoria> findAllByTipoContainingIgnoreCase(String tipo);
 }
