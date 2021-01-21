6bc5103472c909b4ab15b843fcc7114f1201f0e1
Gustavo Miquéias <gumiqueias@hotmail.com>
2021-01-21 15:11:03 -0300

    Componentes Page

--- front_end/projetoIntegrador/src/app/app.module.ts
+++ front_end/projetoIntegrador/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductPageComponent } from './loja/product-page/product-page.component';
+import { ProductsComponent } from './loja/products/products.component';
+import { FiltersComponent } from './loja/filters/filters.component';
+import { MenuComponent } from './home/menu/menu.component';
+import { DestaquesComponent } from './home/destaques/destaques.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntrarComponent,
+    CadastrarComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProductPageComponent,
+    ProductsComponent,
+    FiltersComponent,
+    MenuComponent,
+    DestaquesComponent
   ],
   imports: [
     BrowserModule,
