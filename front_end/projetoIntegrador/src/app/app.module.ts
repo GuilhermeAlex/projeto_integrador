710c48b6b16c193ec3e21772427e852f588eccde
Bruno-H-Moraes <brunohmoraes93@gmail.com>
2021-01-25 10:32:18 -0300

    Navbar all page + destaque

--- front_end/projetoIntegrador/src/app/app.module.ts
+++ front_end/projetoIntegrador/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './navbar-destaques/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProductPageComponent } from './loja/product-page/product-page.component';
 import { ProductsComponent } from './loja/products/products.component';
