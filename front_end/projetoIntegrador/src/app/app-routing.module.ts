1c85823a1e9798e8239951743d586e650aaa9855
Gustavo Miquéias <gumiqueias@hotmail.com>
2021-01-27 12:50:14 -0300

    Rotas

--- front_end/projetoIntegrador/src/app/app-routing.module.ts
+++ front_end/projetoIntegrador/src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { ContatoComponent } from './contato/contato.component';
+import { DestaquesComponent } from './home/destaques/destaques.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
@@ -7,6 +9,8 @@ const routes: Routes = [
   { path: '', redirectTo: 'entrar', pathMatch: 'full' },
   { path: 'entrar', component: EntrarComponent },
   { path: 'cadastrar', component: CadastrarComponent },
+  {path: 'home', component: DestaquesComponent},
+  {path: 'contato', component: ContatoComponent}
 ];
 
 @NgModule({
