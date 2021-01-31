812bed85aed84b2f362a98f50dac8f3391d8dab9
LucasSG00 <lucas_s_goncalves@hotmail.com>
2021-01-31 17:29:52 -0300

    Desenvolvimento da página Sobre

--- front_end/projetoIntegrador/src/app/app-routing.module.ts
+++ front_end/projetoIntegrador/src/app/app-routing.module.ts
@@ -5,14 +5,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { PerfilComponent } from './perfil/perfil.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'entrar', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'entrar', component: EntrarComponent },
   { path: 'cadastrar', component: CadastrarComponent },
   { path: 'home', component: DestaquesComponent},
   { path: 'contato', component: ContatoComponent},
-  { path: 'perfil', component: PerfilComponent }
+  { path: 'perfil', component: PerfilComponent },
+  { path: 'sobre', component: SobreComponent }
 ];
 
 @NgModule({
