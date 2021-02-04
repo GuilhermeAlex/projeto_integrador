b51ebe6c52c3783b1442ae3259db0043b84c41d0
danielferreira3 <daniel.z.ferreira@hotmail.com>
2021-02-04 16:38:13 -0300

    produtos cadastrado

---
+++ MySql/tb_categoria.sql
@@ -0,0 +1,5 @@
+insert into tb_categoria values (DEFAULT,'Papelão',null);
+insert into tb_categoria values (DEFAULT,'Madeira',null); 
+insert into tb_categoria values (DEFAULT,'PET',null); 
+insert into tb_categoria values (DEFAULT,'Pallet',null); 
+insert into tb_categoria values (DEFAULT,'Plastico',null);
