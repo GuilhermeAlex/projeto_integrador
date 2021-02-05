026d1af459a54e3647662f9d3c1879a9f5433bee
Gustavo Miquéias <gumiqueias@hotmail.com>
2021-02-04 21:48:29 -0300

    Att page carrinho e home

--- front_end/projetoIntegrador/src/app/carrinho/carrinho.component.ts
+++ front_end/projetoIntegrador/src/app/carrinho/carrinho.component.ts
@@ -1,42 +1,80 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-carrinho',
   templateUrl: './carrinho.component.html',
-  styleUrls: ['./carrinho.component.css']
+  styleUrls: ['./carrinho.component.css'],
 })
 export class CarrinhoComponent implements OnInit {
+  boleto: boolean = true;
+  cartao: boolean;
 
-  boleto: boolean = true
-  cartao: boolean
+  alertaNome: string;
+  alertaSobrenome: string;
+  alertaCpf: string;
+  alertaRg: string;
+  alertaTel: string;
+  alertaEmail: string;
+  alertaEmailx: string;
 
-  constructor() { }
+  nome: string;
+  sobrenome: string;
+  cpf: string;
+
+  nomeOk: boolean;
+  sobrenomeOk: boolean;
+  cpfOk: boolean;
+
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
-
+    window.scroll(0, 0);
   }
 
-  mostraBoleto(){
-
-    if(this.boleto === true){
-      this.cartao = false
-
-    }else if(this.boleto == false){
-      this.cartao = true
+  mostraBoleto() {
+    if (this.boleto === true) {
+      this.cartao = false;
+    } else if (this.boleto == false) {
+      this.cartao = true;
     }
-    return this.boleto
-
+    return this.boleto;
   }
-    mostraCartao(){
-
-      if(this.boleto === false){
-        this.cartao = true
-
-      }else if(this.boleto == true){
-        this.cartao = false
-      }
-      return this.cartao
+  mostraCartao() {
+    if (this.boleto === false) {
+      this.cartao = true;
+    } else if (this.boleto == true) {
+      this.cartao = false;
     }
+    return this.cartao;
+  }
 
+  validaNome() {
+    if (this.nome.length < 3) {
+      this.nomeOk = false;
+      this.alertaNome = 'Nome inválido';
+    } else {
+      this.nomeOk = true;
+      this.alertaNome = '';
+    }
+  }
+  validaSobrenome() {
+    if (this.sobrenome.length < 3 || this.sobrenome == this.nome) {
+      this.sobrenomeOk = false;
+      this.alertaSobrenome = 'Sobrenome inválido';
+    } else {
+      this.sobrenomeOk = true;
+      this.alertaSobrenome = '';
+    }
+  }
+
+  validaCpf() {
+    if (this.cpf.length < 11 || this.cpf.length > 12) {
+      this.cpfOk = false;
+      this.alertaCpf = 'CPF inválido';
+    } else {
+      this.cpfOk = true;
+      this.alertaCpf = '';
+    }
+  }
 }
